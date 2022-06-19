7701f861e6c5f878d19eaa91cb5e9fa11fc708aa
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-06-20 02:26:56 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
-
-# Ushbu kod Ahmadjon Dadabayev tomonidan qaytadan yozib chiqarildi. uitc.uz platformasi uchun bajarildi 
-# Mualliflik huquqi saqlansin!
-# dadabayev.uz
-# +998902224311
-# @Akhmadjon
-
-# This code was rewritten by Ahmadjon Dadabayev. Done for the uitc.uz platform
-# Copyright reserved!
-# dadabayev.uz
-# +998902224311
-# @Akhmadjon
-
-# Этот код был переписан Ахмаджоном Дадабаевым. Сделано для платформы uitc.uz
-# Авторские права защищены!
-# dadabayev.uz
-# +998902224311
-# @Akhmadjon
-
 error_reporting(0);
 $image      = new CURLFILE("image.jpg"); // image location
 $api        = 'donotstealthiskey3589';
